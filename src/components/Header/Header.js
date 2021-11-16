27149aefb1a049e3cac1c133fb03f3c0ff23decb
Adrian Camacho <adriancamacho18@gmail.com>
2021-11-15 17:50:45 -0800

    add github link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,6 +14,28 @@ const Header = () =>  (
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#tech'>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#about'>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com/chonnessey'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
