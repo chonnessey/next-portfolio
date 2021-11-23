f3ca38c08a2ce791a324b054d2945a2f627fa3f0
Adrian Camacho <adriancamacho18@gmail.com>
2021-11-22 23:25:56 -0800

    add technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,39 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a wide range of languages and frameworks. Below are a list of some I've had the most experience with.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            JavaScript <br/>
+            React <br/>
+            JQuery <br/>
+            TailwindCSS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Node.js <br/>
+            Ruby on Rails <br/>
+            Firebase <br/>
+            Python
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
