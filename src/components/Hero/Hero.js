055412f20a031270321ae6c93f6f5c98d85b4ad4
Adrian Camacho <adriancamacho18@gmail.com>
2021-11-22 19:31:27 -0800

    add hero section and svg animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello There, <br />
+        I'm Adrian Camacho
+      </SectionTitle>
+      <SectionText>
+      Creative and forward-thinking Junior Full Stack Developer with a keen eye in creating intuitive digital design projects. I've spent the last 7 years honing in my soft skills in the service industry and am more than ready to make the transition into web development.
+      </SectionText>
+      <Button onClick={() => window.location = ``}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
